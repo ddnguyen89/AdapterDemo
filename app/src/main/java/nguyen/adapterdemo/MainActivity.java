3a18e1bc8c54585af31628eb79892875f458fe5d
eatdabanunu <duynguyen.xz@gmail.com>
2017-12-04 12:01:11 -0800

    Update: 12/04/2017

--- app/src/main/java/nguyen/adapterdemo/MainActivity.java
+++ app/src/main/java/nguyen/adapterdemo/MainActivity.java
@@ -1,5 +1,7 @@
 package nguyen.adapterdemo;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +10,29 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
 
-    ListView listView;
+import nguyen.adapterdemo.data.Company;
+import nguyen.adapterdemo.data.CompanyData;
+
+public class MainActivity extends ListActivity {
+
+    //old AdapterDemo extends AppCompatActivity
+    //ListView listView;
+
+    private static final int REQUEST_CODE = 100;
+    List<Company> mCompanies = new CompanyData().getmCompanies();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //create the custom adapter and get it
+        CustomAdapter adapter = new CustomAdapter(this, R.layout.item_detail, mCompanies);
+        setListAdapter(adapter);
+
+        /*old AdapterDemo
 
         listView = (ListView) findViewById(R.id.list_item);
 
@@ -45,5 +62,38 @@ public class MainActivity extends AppCompatActivity {
                         Toast.LENGTH_LONG).show();
             }
         });
+        */
+    }
+
+    //this function is used by the adapter
+    @Override
+    protected void onListItemClick(ListView parent, View v, int position, long id) {
+        super.onListItemClick(parent, v, position, id);
+
+        //return the position of the list of companies
+        Company company = mCompanies.get(position);
+
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra("companyName", company.companyName);
+        intent.putExtra("imageResource", company.imageResource);
+        intent.putExtra("phone", company.phone);
+        intent.putExtra("website", company.website);
+        intent.putExtra("email", company.email);
+
+        //sometimes you want to get a result back from an activity when it ends
+        //for example, you may start an activity that lets the user pick a person
+        //in a list of contacts. when it ends, it returns the person that was selected
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    //invoked automattically when second activity returns the result from startActivityForResult
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == REQUEST_CODE && requestCode == RESULT_OK) {
+            String companyName = data.getStringExtra("companyName");
+            String action = data.getStringExtra("action");
+        }
     }
 }
